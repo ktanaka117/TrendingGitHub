df4bfc9bd1fd0acdd5d6d3a6d16c9c60c378d4a4
田中賢治 <key.m4096.kung.fu@gmail.com>
2015-09-16 17:14:23 +0900

    Define Trending class and getTrendingTask method

--- TrendingGitHub/Trending.swift
+++ TrendingGitHub/Trending.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import Alamofire
+import Kanna
+import PromiseKit
+
+class Trending {
+    class func getTrendingTask() -> Promise<[Repository]> {
+        
+        var elementIndex = 0
+        var repositories: [Repository] = []
+        
+        return Promise { fulfill, reject in
+            let request = Alamofire.request(Router.Ranking(language: "swift"))
+            Alamofire.request(Router.Ranking(language: "swift")).response({ request, response, data, error in
+                if let error = error {
+                    reject(error)
+                } else {
+                    if let doc = Kanna.HTML(html: data!, encoding: NSUTF8StringEncoding) {
+                        for node in doc.css(".repo-list-item") {
+                            let repoListName = dropUnneccessaryElement(doc.css(".repo-list-name")[elementIndex].text!)
+                            let repoListDescription = dropUnneccessaryElement(doc.css(".repo-list-description")[elementIndex].text!)
+                            
+                            var repository = Repository()
+                            repository.title = repoListName
+                            repository.description = repoListDescription
+                            repositories.append(repository)
+                            
+                            elementIndex++
+                        }
+                        fulfill(repositories)
+                    }
+                }
+            })
+        }
+    }
+}
+
+func dropUnneccessaryElement(text: String) -> String {
+    let nDrop = text.stringByReplacingOccurrencesOfString("\n", withString: "", options: nil, range: nil)
+    let spaceDrop = nDrop.stringByReplacingOccurrencesOfString(" ", withString: "", options: nil, range: nil)
+    
+    return spaceDrop
+}
