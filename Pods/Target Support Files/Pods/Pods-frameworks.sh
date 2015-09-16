ea742803ad9192169e914c4ae880c78532a64509
田中賢治 <key.m4096.kung.fu@gmail.com>
2015-09-16 16:29:56 +0900

    Add PromiseKit

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -55,11 +55,15 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/Alamofire.framework'
   install_framework 'Pods/FLAnimatedImage.framework'
   install_framework 'Pods/Kanna.framework'
+  install_framework 'Pods/OMGHTTPURLRQ.framework'
+  install_framework 'Pods/PromiseKit.framework'
   install_framework 'Pods/SwiftyJSON.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/Alamofire.framework'
   install_framework 'Pods/FLAnimatedImage.framework'
   install_framework 'Pods/Kanna.framework'
+  install_framework 'Pods/OMGHTTPURLRQ.framework'
+  install_framework 'Pods/PromiseKit.framework'
   install_framework 'Pods/SwiftyJSON.framework'
 fi
