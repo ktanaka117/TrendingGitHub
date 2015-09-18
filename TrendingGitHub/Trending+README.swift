fb7c6d17768f8695156260da91fd2cb533db5844
田中賢治 <key.m4096.kung.fu@gmail.com>
2015-09-18 16:20:16 +0900

    Define getReadmeTask

--- TrendingGitHub/Trending+README.swift
+++ TrendingGitHub/Trending+README.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import Alamofire
+import Kanna
+import PromiseKit
+
+extension Trending {
+    
+    class func getReadmeTask() -> Promise<String> {
+        return Promise { fulfill, reject in
+            let request = Alamofire.request(Router.Readme(builder: "Alamofire", repositoryTitle: "Alamofire"))
+            request.response({ request, response, data, error in
+                if let error = error {
+                    reject(error)
+                } else {
+                    if let data = data {
+                        let markdownStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                        fulfill(markdownStr as! String)
+                    }
+                }
+            })
+        }
+    }
+}
